9a3192cebfb9b42a9250cb2b3ba574a7df76a1e5
Grzegorz Lachowski <gregory.lachowski@gmail.com>
2020-05-31 19:26:50 +0200

    impl basic copy and show

---
+++ index.js
@@ -0,0 +1,85 @@
+const inquirer = require('inquirer');
+const {
+    exec
+} = require('child_process');
+
+const glob = require('glob');
+const path = require('path');
+
+function findEntry(passwordStoreDir, phrase) {
+    const pattern = path.join("**", "*" + phrase + "*", "**", "*.gpg");
+    const matches = glob.sync(pattern, {
+        nocase: true,
+        cwd: passwordStoreDir
+    });
+    return matches.map(x => x.replace(/\.gpg$/, ""));
+}
+
+inquirer.registerPrompt('autocomplete', require('inquirer-autocomplete-prompt'));
+
+function getPasswordStoreDir() {
+    return path.join(process.env.HOME, ".password-store");
+}
+async function main() {
+
+    const passStoreDir = getPasswordStoreDir();
+    const answer = await inquirer.prompt([
+        {
+            type: 'autocomplete',
+            name: 'entry',
+            message: 'Find: ',
+            source: async function (answersSoFar, input) {
+                if (!input) {
+                    return [];
+                }
+
+                return findEntry(passStoreDir, input);
+            }
+        },
+        {
+            type: 'list',
+            name: 'action',
+            message: 'Action: ',
+            default: "copy",
+            choices: [
+                {
+                    name: "Copy",
+                    value: "copy"
+                },
+                {
+                    name: "Show",
+                    value: "show"
+                }
+            ]
+        }
+    ]);
+
+    const PASS_BIN = "/usr/bin/pass"
+    let cmdAction;
+    switch (answer.action) {
+        case "copy":
+            cmdAction = "show -c"
+            break;
+        case "show":
+            cmdAction = "show"
+            break;
+        default:
+            throw new Error(`Not support action ${answer.action}.`);
+    }
+
+    
+    const command = `${PASS_BIN} ${cmdAction} ${answer.entry}`;
+    const child = exec(command, (err, stdout, stderr) => {
+        if (err) {
+            console.error(stderr);
+            process.exit(1);
+            return;
+        }
+
+        console.log(stdout);
+    });
+
+    // in case of copy we need to detach from terminal
+}
+
+main();
